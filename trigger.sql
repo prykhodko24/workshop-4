bff5f229bf6ec85a09c8db2428d0f2a6d2b40bc3
tanya-24-01-2001 <44785259+tanya-24-01-2001@users.noreply.github.com>
2020-05-24 22:14:33 +0300

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -3,6 +3,7 @@ AS SELECT Stores.store_name,visits.TIME_OF_VISIT
 From stores
 JOIN visits On Stores.STORE_NUMBER=visits.STORE_NUMBER
 ;
+
 Create or REPLACE TRIGGER change_name_stoe_trg
 
 BEFORE
@@ -11,13 +12,13 @@ BEFORE
 FOR EACH ROW
 declare
      count_visits_store number;
-     old_ stores.store_name%type;
+     old_ VARCHAR2(100);
 begin
-old_:= :old.store_name;
+--old_:= :old.store_name;
 
 Select count(*) into count_visits_store
 From store_visit
-where store_name = old_
+where store_name  = :old.store_name
 group by store_name;
 
 IF ( count_visits_store > 0) THEN
@@ -26,3 +27,7 @@ IF ( count_visits_store > 0) THEN
 
 END IF;
 end;
+
+UPDATE stores
+SET store_name = 'Nation'
+WHERE store_name='Nation Towers';
