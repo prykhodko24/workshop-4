423e74bbe9adc20a6190b16548c091009a16c436
tanya-24-01-2001 <44785259+tanya-24-01-2001@users.noreply.github.com>
2020-05-24 21:15:16 +0300

    Create trigger.sql

---
+++ trigger.sql
@@ -0,0 +1,28 @@
+CREATE VIEW store_visit
+AS SELECT Stores.store_name,visits.TIME_OF_VISIT
+From stores
+JOIN visits On Stores.STORE_NUMBER=visits.STORE_NUMBER
+;
+Create or REPLACE TRIGGER change_name_stoe_trg
+
+BEFORE
+    update of store_name
+        on stores
+FOR EACH ROW
+declare
+     count_visits_store number;
+     old_ stores.store_name%type;
+begin
+old_:= :old.store_name;
+
+Select count(*) into count_visits_store
+From store_visit
+where store_name = old_
+group by store_name;
+
+IF ( count_visits_store > 0) THEN
+
+        RAISE_APPLICATION_ERROR(-20001, 'Can`t change');
+
+END IF;
+end;
