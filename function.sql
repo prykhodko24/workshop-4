b2c605d497015cc70242e04e4d33ed09d64f1376
tanya-24-01-2001 <44785259+tanya-24-01-2001@users.noreply.github.com>
2020-05-29 09:46:34 +0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,30 +1,26 @@
-create type TypeFunkObject as object
-(PEOPLE_NAME varchar2(500),
- COUNTRY_NAME varchar2(10));
+create type TypeFuncObject as object
+(PEOPLE_NAME varchar2(500),COUNTRY_NAME varchar2(10));
 
-create type TypeFunkList as table of TypeFunkObject;
+create type TypeFuncList as table of TypeFuncObject;
 
 
 CREATE OR REPLACE FUNCTION visiter_country(counry1 in varchar2,country2 in varchar2)
-          return TypeFunkList pipelined as
-                       
-                       
+          return TypeFuncList pipelined as
+                                                 
 begin
-                       
-                       
-  for i in (
-    SELECT COUNTRY.COUNTRY_NAME,PEOPLE.PEOPLE_NAME
+                                                 
+  for i in (SELECT COUNTRY.COUNTRY_NAME,PEOPLE.PEOPLE_NAME
     FROM PEOPLE
     join VISITS on PEOPLE.PEOPLE_NAME=VISITS.PEOPLE_NAME
     join STORES on VISITS.STORE_NUMBER=STORES.STORE_NUMBER
     join CITY on STORES.CITY_NAME=CITY.CITY_NAME
     join COUNTRY on city.COUNTRY_NAME=COUNTRY.COUNTRY_NAME
     WHERE COUNTRY.COUNTRY_NAME=counry1 or COUNTRY.COUNTRY_NAME=country2)
-                       
-                       
+                                                 
+                                                 
  loop
-   pipe row (TypeFunkObject(i.PEOPLE_NAME, i.COUNTRY_NAME));    
+   pipe row (TypeFuncObject(i.PEOPLE_NAME, i.COUNTRY_NAME));    
   end loop;
-                            
+  
  return;
 end;
